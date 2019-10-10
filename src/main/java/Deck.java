04e8c6c5f14bf5b1c396bcef7fdb263fcad7aa73
Allan Milne <amilne.wd@gmail.com>
2019-10-10 14:38:08 +0100

    Can Shuffle

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 
-    private ArrayList<Card> deck;
+        private ArrayList<Card> deck;
 
-    public Deck(){
-        this.deck = new ArrayList<>();
-    }
-
-    public int cardCount(){
-        return this.deck.size();
-    }
-
-    public void addCard(Card card){
-        this.deck.add(card);
-    }
-
-    public void newDeck(){
-        for (SuitType eachsuit : SuitType.values()){
-            for (RankType eachrank : RankType.values()){
-                Card card = new Card(eachsuit, eachrank);
-                deck.add(card);
-            }
+    public Deck() {
+            this.deck = new ArrayList<>();
         }
-    }
+
+        public int cardCount () {
+            return this.deck.size();
+        }
+
+        public void addCard (Card card){
+            this.deck.add(card);
+        }
+
+        public void newDeck () {
+            for (SuitType eachsuit : SuitType.values()) {
+                for (RankType eachrank : RankType.values()) {
+                    Card card = new Card(eachsuit, eachrank);
+                    deck.add(card);
+                }
+            }
+            System.out.println(deck);
+            Collections.shuffle(deck);
+            System.out.println(deck);
+
+        }
 }
